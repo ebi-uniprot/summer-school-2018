43fdb93b1a2107e838bc06002f7876ecd311c70a
Vladimir <vladimir@ebi.ac.uk>
2018-06-26 15:09:14 +0100

    some samples to test json parsing on

---
+++ test-load-protein.py
@@ -0,0 +1,9 @@
+import json
+
+from load_protein_info import *
+
+# print(read_protein('P36843'))
+# print(read_protein('Q9FBI2'))
+# print(read_protein('E1V682'))
+# print(read_protein('P0CK01'))
+print(read_protein('P19560'))
